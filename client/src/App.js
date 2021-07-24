383c788d60597a1e5d960c7d7d0c6c4b24720fc3
sardorexe <vapaevsad@gmail.com>
2021-07-25 03:08:48 +0500

    Just code it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import  Chat  from './components/Chat/Chat';
+import  Join  from './components/Join/Join';
+import "./App.css"
 
 function App() {
   return (
     <Router>
       <Switch>
-        
+        <Route exact path="/">
+          <Join />
+        </Route>
+        <Route exact path="/chat">
+          <Chat />
+        </Route>
       </Switch>
     </Router>
   );
--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import "./Join.css"
+import React from "react";
+import "./Join.css";
 
-export const Join = () => {
-    return (
-        <div className="join">
-            <h1>Join</h1>
+const Join = () => {
+  return (
+    <div className="join">
+      <div className="join-form">
+        <div className="form-start">
+          <h2 className="form-icon">
+            <i class="fas fa-paper-plane"></i>
+          </h2>
+          <h3 className="form-t">Let's chat with your friends!</h3>
         </div>
-    )
-}
+        <form className="form-register">
+          <input type="text" placeholder="Username" className="register-inp global" />
+          <input type="text" placeholder="Room" className="register-inp global" />
+          <button type="submit" className="register-btn global">
+            Join
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
